933540ceb6c074ad3f6be3dc43de5c13786073c8
Abhinav <abhirealme55@gmail.com>
2022-08-20 21:52:57 +0530

    code aligned

--- calculate.js
+++ calculate.js
@@ -1,75 +1,62 @@
-var flag=0
+var flag = 0
 var oldValue
 var newNum
 var math
 
-
- 
-        
-  
-
-function btnClickclear(valC){
-    document.getElementById("screen").value=valC
+function btnClickclear(valC) {
+    document.getElementById("screen").value = valC
 }
-function btnOperation(opr){
-    flag=1
-    
-        oldValue=document.getElementById("screen").value
-        if(opr=="+"){
-            math=1
-            
-        }else if(opr=="-"){
-math=2
-        }if(opr=="*"){
-            math=3
-            
-        }if(opr=="/"){
-            math=4
-            
-        }
+function btnOperation(opr) {
+    flag = 1
+    oldValue = document.getElementById("screen").value
+    if (opr == "+") {
+        math = 1
+    } else if (opr == "-") {
+        math = 2
+    } if (opr == "*") {
+        math = 3
+    } if (opr == "/") {
+        math = 4
+    }
 }
-function equalClick(){
-    if(math=="1"){
+function equalClick() {
+    if (math == "1") {
+        var newValue = document.getElementById("screen").value
+        var oldNum = parseFloat(oldValue)
+        newNum = parseFloat(newValue)
+        var res = oldNum + newNum
+        document.getElementById("screen").value = res
+        math = 0
 
-    var newValue=document.getElementById("screen").value
-    var oldNum=parseFloat(oldValue)
-     newNum=parseFloat(newValue)
-    var res=oldNum+newNum
-    document.getElementById("screen").value=res
-    math=0
-    
-    }if(math=="2"){
+    } if (math == "2") {
+        var newValue = document.getElementById("screen").value
+        var oldNum = parseFloat(oldValue)
+        newNum = parseFloat(newValue)
+        var res = oldNum - newNum
+        document.getElementById("screen").value = res
+        math = 0
+    } if (math == "3") {
+        var newValue = document.getElementById("screen").value
+        var oldNum = parseFloat(oldValue)
+        newNum = parseFloat(newValue)
+        var res = oldNum * newNum
+        document.getElementById("screen").value = res
+        math = 0
+    } if (math == "4") {
+        var newValue = document.getElementById("screen").value
+        var oldNum = parseFloat(oldValue)
+        newNum = parseFloat(newValue)
+        var res = oldNum / newNum
+        document.getElementById("screen").value = res
+        math = 0
+    }
 
-        var newValue=document.getElementById("screen").value
-        var oldNum=parseFloat(oldValue)
-         newNum=parseFloat(newValue)
-        var res=oldNum-newNum
-        document.getElementById("screen").value=res
-        math=0
-        }if(math=="3"){
-
-            var newValue=document.getElementById("screen").value
-            var oldNum=parseFloat(oldValue)
-             newNum=parseFloat(newValue)
-            var res=oldNum*newNum
-            document.getElementById("screen").value=res
-            math=0
-            }if(math=="4"){
-
-                var newValue=document.getElementById("screen").value
-                var oldNum=parseFloat(oldValue)
-                 newNum=parseFloat(newValue)
-                var res=oldNum/newNum
-                document.getElementById("screen").value=res
-                math=0
-                }
-                
 }
-function btnClick(val){
-    if(flag=="1"){
-        document.getElementById("screen").value=val;
-        flag=0
-    }else{
-        document.getElementById("screen").value=document.getElementById("screen").value+val 
+function btnClick(val) {
+    if (flag == "1") {
+        document.getElementById("screen").value = val;
+        flag = 0
+    } else {
+        document.getElementById("screen").value = document.getElementById("screen").value + val
     }
 }
